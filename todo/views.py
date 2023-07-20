ced6fecbf1746c893fe2bf5d1cdc9b4c17155b35
1F10220253 <s1f102202538@iniad.org>
2023-07-20 12:05:00 +0900

    comment機能を追加

--- todo/views.py
+++ todo/views.py
@@ -8,7 +8,7 @@ from todo.models import Task
 # Create your views here.
 def index(request):
     if request.method == 'POST':
-        task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        task = Task(title=request.POST['title'], comment=request.POST['comment'], due_at=make_aware(parse_datetime(request.POST['due_at'])))
         task.save()
 
     if request.GET.get('order') == 'due':
@@ -41,6 +41,7 @@ def update(request, task_id):
 
     if request.method == 'POST':
         task.title = request.POST['title']
+        task.comment = request.POST['comment']
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
         task.save()
         return redirect(detail, task_id)
